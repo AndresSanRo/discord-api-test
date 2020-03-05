14efb9e5a8c08e80468bd1a57f90d5a2b377643f
Andrés <andressanchezro@hotmail.com>
2020-03-05 21:57:23 +0100

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/App.css';
+import Principal from './components/principal';
+
+function App() {
+  return (
+    <Principal />
+  );
+}
+
+export default App;
---
+++ src/components/principal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Principal() {
+    return (
+        <>
+            <button>Holi</button>
+        </>
+    );
+  }
+  
+  export default Principal;
